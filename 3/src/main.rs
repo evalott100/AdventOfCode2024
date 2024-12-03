0fe19c5c5b9c6315e1dad811e19a741878aa4705
Eva Lott <evalott100@gmail.com>
2024-12-03 22:04:23 +0000

    played around with match statements

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -1,41 +1,46 @@
 use regex::Regex;
 use std::fs;
 
-fn solution_1(path: &str) -> i32 {
+fn load_input(path: &str) -> String {
+    fs::read_to_string(path).expect("Failed to read file")
+}
+
+fn solution_1(input: &str) -> i32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let content = fs::read_to_string(path).expect("Failed to read file");
+
     let mut sum: i32 = 0;
 
-    for cap in re.captures_iter(&content) {
-        let n: i32 = cap[1].parse().unwrap();
-        let m: i32 = cap[2].parse().unwrap();
-        sum += n * m;
+    for capture in re.captures_iter(input) {
+        sum += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap();
     }
     sum
 }
 
-fn solution_2(path: &str) -> i32 {
+fn solution_2(input: &str) -> i32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
-    let content = fs::read_to_string(path).expect("Failed to read file");
+
     let mut do_flag: bool = true;
     let mut sum: i32 = 0;
 
-    for cap in re.captures_iter(&content) {
-        if let Some(mul_cap) = cap.get(1) {
-            if !do_flag {
-                continue;
+    for capture in re.captures_iter(input) {
+        match (do_flag, &capture[0]) {
+            (_, "do()") => {
+                do_flag = true;
             }
-            let n: i32 = mul_cap.as_str().parse().unwrap();
-            let m: i32 = cap[2].parse().unwrap();
-            sum += n * m;
-        } else {
-            do_flag = cap.get(0).unwrap().as_str() == "do()";
+            (_, "don't()") => {
+                do_flag = false;
+            }
+            (true, _) => {
+                sum += capture[1].parse::<i32>().unwrap() * capture[2].parse::<i32>().unwrap();
+            }
+            (_, _) => {}
         }
     }
     sum
 }
 
 fn main() {
-    println!("solution_1: {}", solution_1("input.dat"));
-    println!("solution_2: {}", solution_2("input.dat"));
+    let input = load_input("input.dat");
+    println!("solution_1: {}", solution_1(&input));
+    println!("solution_2: {}", solution_2(&input));
 }
