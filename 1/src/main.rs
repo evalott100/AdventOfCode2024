8387f6c223fe8de9b2634bba2653056bab0f2c3b
Eva Lott <evalott100@gmail.com>
2024-12-03 18:39:41 +0000

    reran formatting

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,11 +1,12 @@
-use std::fs::{File};
+use std::collections::HashMap;
+use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
-
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
