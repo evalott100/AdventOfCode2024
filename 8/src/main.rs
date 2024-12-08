fc5c20904d00232ec853449c58b3b093f1bbcf47
Eva Lott <evalott100@gmail.com>
2024-12-08 12:19:13 +0000

    cleaned up day 8

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
@@ -7,60 +7,122 @@ type Antenna = char;
 type AntennaGrid = Vec<Vec<Antenna>>;
 const EMPTY_ANTENNA: Antenna = '.';
 
+#[derive(Hash, Eq, PartialEq, Copy, Clone)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Position {
+        Position { x, y }
+    }
+
+    fn in_bounds(&self, x_bounds: i32, y_bounds: i32) -> bool {
+        self.x >= 0 && self.x <= x_bounds && self.y >= 0 && self.y <= y_bounds
+    }
+}
+
+impl From<(usize, usize)> for Position {
+    fn from(coords: (usize, usize)) -> Self {
+        Position::new(coords.0 as i32, coords.1 as i32)
+    }
+}
+
+impl From<(Position, Position)> for Position {
+    fn from(positions: (Position, Position)) -> Self {
+        Position::new(
+            2 * positions.1.x - positions.0.x,
+            2 * positions.1.y - positions.0.y,
+        )
+    }
+}
+
 fn load_input(path: &str) -> AntennaGrid {
-    let mut antenna_uuids: HashMap<char, Antenna> = HashMap::from([('.', EMPTY_ANTENNA)]);
     let mut antenna_grid: AntennaGrid = Vec::new();
 
     let file = File::open(path).expect("failed to open file");
     let reader = BufReader::new(file);
     for line in reader.lines().filter_map(|line| line.ok()) {
-        let mut antenna_grid_y = Vec::new();
-        for character in line.chars() {
-            let antenna = antenna_uuids.entry(character).or_insert(character);
-            antenna_grid_y.push(*antenna);
-        }
-        antenna_grid.push(antenna_grid_y);
+        antenna_grid.push(line.chars().collect());
     }
+
     antenna_grid
 }
 
-fn in_bounds(x: i32, y: i32, x_bounds: i32, y_bounds: i32) -> bool {
-    x >= 0 && x <= x_bounds && y >= 0 && y <= y_bounds
-}
-
-fn calculate_new_position(x_1: i32, y_1: i32, x_2: i32, y_2: i32) -> (i32, i32) {
-    (2 * x_2 - x_1, 2 * y_2 - y_1)
-}
-
-fn calculate_antinodes_with_antenna(
+fn calculate_antenna_antinodes(
     antenna: Antenna,
-    antenna_x: usize,
-    antenna_y: usize,
+    position: Position,
     antenna_grid: &AntennaGrid,
-) -> HashSet<(usize, usize)> {
-    let mut antinode_locations: HashSet<(usize, usize)> = HashSet::new();
+) -> HashSet<Position> {
+    let mut antinode_locations: HashSet<Position> = HashSet::new();
 
     for (y, x_vector) in antenna_grid.iter().enumerate() {
-        for (x, coordinate) in x_vector.iter().enumerate() {
-            if (x, y) == (antenna_x, antenna_y) || *coordinate != antenna {
-                continue;
+        for other_position in x_vector.iter().enumerate().filter_map(|(x, coordinate)| {
+            let other_position = Position::from((x, y));
+            if *coordinate == antenna && other_position != position {
+                Some(other_position)
+            } else {
+                None
             }
+        }) {
+            let calculated = Position::from((position, other_position));
 
-            let (old_x, old_y) = (antenna_x as i32, antenna_y as i32);
-            let (new_x, new_y) = (x as i32, y as i32);
-
-            let (calculated_x, calculated_y) = calculate_new_position(old_x, old_y, new_x, new_y);
-
-            if in_bounds(
-                calculated_x,
-                calculated_y,
-                (x_vector.len() - 1) as i32,
-                (antenna_grid.len() - 1) as i32,
-            ) {
-                antinode_locations.insert((calculated_x as usize, calculated_y as usize));
+            if calculated.in_bounds((x_vector.len() - 1) as i32, (antenna_grid.len() - 1) as i32) {
+                antinode_locations.insert(calculated);
             }
         }
     }
+
+    antinode_locations
+}
+
+fn calculate_antenna_antinodes_repeating(
+    antenna: Antenna,
+    position: Position,
+    antenna_grid: &AntennaGrid,
+) -> HashSet<Position> {
+    let mut antinode_locations: HashSet<Position> = HashSet::new();
+
+    for (y, x_vector) in antenna_grid.iter().enumerate() {
+        for other_position in x_vector.iter().enumerate().filter_map(|(x, coordinate)| {
+            if *coordinate == antenna {
+                let other_position = Position::from((x, y));
+                Some(other_position)
+            } else {
+                None
+            }
+        }) {
+            let mut old = position;
+            let mut new = other_position;
+
+            if new == old {
+                antinode_locations.insert(other_position);
+                continue;
+            }
+
+            loop {
+                let calculated = Position::from((old, new));
+
+                if !calculated
+                    .in_bounds((x_vector.len() - 1) as i32, (antenna_grid.len() - 1) as i32)
+                {
+                    break;
+                }
+
+                antinode_locations.insert(calculated);
+
+                if calculated == new {
+                    // Only one node on the antenna itself
+                    break;
+                }
+
+                old = new;
+                new = calculated;
+            }
+        }
+    }
+
     antinode_locations
 }
 
@@ -70,61 +132,16 @@ fn solution_1(antenna_grid: &AntennaGrid) -> u32 {
     for (y, x_vector) in antenna_grid.iter().enumerate() {
         for (x, antenna) in x_vector.iter().enumerate() {
             if *antenna != EMPTY_ANTENNA {
-                antinode_locations.extend(calculate_antinodes_with_antenna(
+                antinode_locations.extend(calculate_antenna_antinodes(
                     *antenna,
-                    x,
-                    y,
+                    Position::from((x, y)),
                     antenna_grid,
                 ))
             }
         }
     }
+
     antinode_locations.len() as u32
-}
-
-fn calculate_antinodes_repeating(
-    antenna_x: usize,
-    antenna_y: usize,
-    antenna_grid: &AntennaGrid,
-) -> HashSet<(usize, usize)> {
-    let mut antinode_locations: HashSet<(usize, usize)> = HashSet::new();
-
-    for (y, x_vector) in antenna_grid.iter().enumerate() {
-        for (x, coordinate) in x_vector.iter().enumerate() {
-            if *coordinate == EMPTY_ANTENNA || (x, y) == (antenna_x, antenna_y) {
-                continue;
-            }
-
-            let (mut old_x, mut old_y) = (antenna_x as i32, antenna_y as i32);
-            let (mut new_x, mut new_y) = (x as i32, y as i32);
-
-            loop {
-                let (calculated_x, calculated_y) =
-                    calculate_new_position(old_x, old_y, new_x, new_y);
-
-                if !in_bounds(
-                    calculated_x,
-                    calculated_y,
-                    (x_vector.len() - 1) as i32,
-                    (antenna_grid.len() - 1) as i32,
-                ) {
-                    break;
-                }
-
-                //println!("found {}, {}", calculated_x + 1, calculated_y + 1);
-                antinode_locations.insert((calculated_x as usize, calculated_y as usize));
-
-                if (calculated_x, calculated_y) == (new_x, new_y) {
-                    // Only one node on the antenna itself
-                    break;
-                }
-
-                (old_x, old_y) = (new_x, new_y);
-                (new_x, new_y) = (calculated_x, calculated_y);
-            }
-        }
-    }
-    antinode_locations
 }
 
 fn solution_2(antenna_grid: &AntennaGrid) -> u32 {
@@ -133,30 +150,21 @@ fn solution_2(antenna_grid: &AntennaGrid) -> u32 {
     for (y, x_vector) in antenna_grid.iter().enumerate() {
         for (x, antenna) in x_vector.iter().enumerate() {
             if *antenna != EMPTY_ANTENNA {
-                antinode_locations.extend(calculate_antinodes_repeating(x, y, antenna_grid))
+                antinode_locations.extend(calculate_antenna_antinodes_repeating(
+                    *antenna,
+                    Position::from((x, y)),
+                    antenna_grid,
+                ))
             }
         }
     }
-    for (y, x_vector) in antenna_grid.iter().enumerate() {
-        for (x, antenna) in x_vector.iter().enumerate() {
-            if *antenna == EMPTY_ANTENNA {
-                if antinode_locations.contains(&(x, y)) {
-                    print!("#");
-                } else {
-                    print!("{}", EMPTY_ANTENNA);
-                }
-            } else {
-                print!("{}", antenna);
-            }
-        }
-        println!();
-    }
+
     antinode_locations.len() as u32
 }
 
 fn main() {
     let input_start = Instant::now();
-    let antenna_grid: AntennaGrid = load_input("input_simple.dat");
+    let antenna_grid: AntennaGrid = load_input("input.dat");
     println!("input took {:?}", input_start.elapsed());
 
     let solution_1_start = Instant::now();
@@ -167,7 +175,6 @@ fn main() {
         solution_1_start.elapsed()
     );
 
-    // NOT 901 (too low)
     let solution_2_start = Instant::now();
     let output_2 = solution_2(&antenna_grid);
     println!(
